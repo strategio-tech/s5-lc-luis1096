c963c11ec09fa81dc41257dfa215a93fbc107084
Luis <luisqaz222@gmail.com>
2022-10-24 15:15:54 -0400

    method implemented
    implemented getRemainingAmountIn3Months method

--- src/main/java/tech/strategio/LoanCalculator.java
+++ src/main/java/tech/strategio/LoanCalculator.java
@@ -2,22 +2,30 @@ package tech.strategio;
 
 import java.util.Scanner;
 
-public class LoanCalculator {
+public class LoanCalculator 
+{
 
     /**
-     * TODO: finish out this JavaDoc comment block.
-     * FIXME: WHAT DOES THIS METHOD DO?
+     * Method that takes a loan amount and returns the remaining 
+     * amount after 3 iterations of 10% deductions. 
      * 
-     * @param ???
-     * @return ???
+     * @param amount the original loan amount 
+     * @return the final loan amount after deductions 
      */
-    static int getRemainingAmountIn3Months(int amount) {
-        // TODO: Rewrite this method
-        return -1;
+    static int getRemainingAmountIn3Months(int amount) 
+    {
+
+        for(int i = 0; i < 3; i++)
+        {
+            amount = amount - (int)(amount * 0.1); 
+        }
+         
+        return amount;
     }
 
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
+        try (Scanner scanner = new Scanner(System.in)) 
+        {
             int amount = scanner.nextInt();
             System.out.println(getRemainingAmountIn3Months(amount));
         }
